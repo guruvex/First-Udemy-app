2f865836d0b571f6b652a2d3cbc70e55ef1b6292
james heath <guruvex@gmail.com>
2018-12-13 16:22:11 -0700

    added projects on

--- app/src/main/java/e/android/simpletoast/MainActivity.java
+++ app/src/main/java/e/android/simpletoast/MainActivity.java
@@ -1,14 +1,22 @@
 package e.android.simpletoast;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * simple first app for Udemy android course.
  * 12/5/2018
+ * to save space and time i am doing the first few simple projects in one
+ * i have put a comment on each part as its added on.
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -17,11 +25,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // first project
         Button myButton = findViewById(R.id.button);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // added a second project
+        SeekBar seekBar = findViewById(R.id.seek);
+        final TextView textView = findViewById(R.id.textView);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView.setText("value"+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        //third project
+        final LinearLayout layout = findViewById(R.id.layout);
+        ToggleButton tButton = findViewById(R.id.tbutton);
+
+        tButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    layout.setBackgroundColor(Color.RED);
+                } else {
+                    layout.setBackgroundColor(Color.GREEN);
+                }
+
             }
         });
     }
